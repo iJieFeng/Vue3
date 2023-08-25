f5cfbc19880fa9eb9552a3573d8b2c4a5a74f87c
oujiefeng <oujiefeng@9kd,com>
2023-08-26 01:54:55 +0800

    ts拓展声明全局属性

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    $imgUrl: string
+  }
+}
+export {}
--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
+app.config.globalProperties.$imgUrl = 'https://cdn.staticaly.com/gh/iJieFeng/pic-bed@main'
 app.use(createPinia())
 app.use(router)
 
